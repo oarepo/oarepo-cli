c664e19b7127afb2618f7be9aeb39088722ce965
Mirek Simek <miroslav.simek@gmail.com>
2023-01-09 07:28:52 +0100

    cookiecutter defaults

--- oarepo_cli/cli/ui/add.py
+++ oarepo_cli/cli/ui/add.py
@@ -11,6 +11,7 @@ from oarepo_cli.config import MonorepoConfig
 from oarepo_cli.ui.wizard import StaticWizardStep, Wizard
 from oarepo_cli.ui.wizard.steps import InputWizardStep, RadioWizardStep, WizardStep
 from oarepo_cli.utils import print_banner, to_python_name
+from os.path import relpath
 
 
 @click.command(
@@ -26,16 +27,23 @@ from oarepo_cli.utils import print_banner, to_python_name
 )
 @click.argument("name")
 def add_ui(project_dir, name, *args, **kwargs):
-    oarepo_yaml_file = project_dir / "oarepo.yaml"
-    cfg = MonorepoConfig(oarepo_yaml_file, section=["uis", name])
-    cfg.load()
-    print_banner()
-    if not (name.endswith("-ui") or name.endswith("-app")):
+    if not (name.endswith("ui") or name.endswith("app")):
         name = name + "-ui"
 
-    cfg["ui_name"] = name
+    oarepo_yaml_file = project_dir / "oarepo.yaml"
+    cfg = MonorepoConfig(oarepo_yaml_file, section=["ui", name])
+    cfg.load()
+    print_banner()
 
     add_ui_wizard(cfg).run(cfg)
+
+
+class UIWizardMixin:
+    def ui_name(self, data):
+        return data.section
+
+    def ui_dir(self, data):
+        return data.project_dir / "ui" / self.ui_name(data)
 
 
 def get_available_models(data):
@@ -51,28 +59,19 @@ def snail_to_title(v):
     return "".join(ele.title() for ele in v.split("_"))
 
 
-class AddUIWizardStep(ProjectWizardMixin, WizardStep):
-    def model_defaults(self, data):
-        ui_name = data["ui_name"]
+class AddUIWizardStep(UIWizardMixin, ProjectWizardMixin, WizardStep):
+    def after_run(self, data):
+        ui_name = self.ui_name(data)
 
         ui_package = to_python_name(ui_name)
         ui_base = snail_to_title(ui_package)
 
-        model_config = data.whole_data["models"][data["model_name"]]["config"]
+        model_config = data.whole_data["models"][data["model_name"]]
         model_package = model_config["model_package"]
 
-        data.setdefault("local_model_path", "../../models/" + data["model_name"])
-        data.setdefault("model_package", model_package)
+        model_path = data.project_dir / model_config["model_dir"]
         model_file = (
-            (
-                data.project_dir
-                / "ui"
-                / ui_name
-                / data["local_model_path"]
-                / model_package
-                / "models"
-                / "model.json"
-            )
+            (model_path / model_package / "models" / "model.json")
             .absolute()
             .resolve(strict=False)
         )
@@ -85,30 +84,43 @@ class AddUIWizardStep(ProjectWizardMixin, WizardStep):
         model_service = model_description["settings"]["python"][
             "proxies-current-service"
         ]
+        cookiecutter_data = {
+            "model_name": data["model_name"],
+            "local_model_path": data.get(
+                "cookiecutter_local_model_path", relpath(model_path, self.ui_dir(data))
+            ),
+            "model_package": data.get("cookiecutter_model_package", model_package),
+            "app_name": data.get("cookiecutter_app_name", ui_name),
+            "app_package": data.get("cookiecutter_app_package", ui_package),
+            "ext_name": data.get("cookiecutter_ext_name", f"{ui_base}AppExtension"),
+            "author": data.get(
+                "cookiecutter_author", model_config.get("author_name", "")
+            ),
+            "author_email": data.get(
+                "cookiecutter_author_email", model_config.get("author_email", "")
+            ),
+            "repository_url": data.get("cookiecutter_repository_url", ""),
+            # TODO: take this dynamically from the running model's Ext so that
+            # TODO: it does not have to be specified here
+            "resource": data.get("cookiecutter_resource", f"{ui_base}Resource"),
+            "resource_config": data.get(
+                "cookiecutter_resource_config", f"{ui_base}ResourceConfig"
+            ),
+            "api_config": data.get("cookiecutter_api_config", model_resource_config),
+            "api_service": data.get("cookiecutter_api_service", model_service),
+            "url_prefix": data["url_prefix"],
+        }
 
-        data.setdefault("app_name", ui_name)
-        data.setdefault("app_package", ui_package)
-        data.setdefault("ext_name", f"{ui_base}AppExtension")
-        data.setdefault("author", data.get("config.author_name", ""))
-        data.setdefault("author_email", data.get("config.author_email", ""))
-        data.setdefault("repository_url", "")
-        # TODO: take this dynamically from the running model's Ext so that
-        # TODO: it does not have to be specified here
-        data.setdefault("resource", f"{ui_base}Resource")
-        data.setdefault("resource_config", f"{ui_base}ResourceConfig")
-        data.setdefault("api_config", model_resource_config)
-        data.setdefault("api_service", model_service)
-
-    def after_run(self, data):
         cookiecutter(
             "https://github.com/oarepo/cookiecutter-app",
             checkout="v10.0",
             no_input=True,
             output_dir=data.project_dir / "ui",
-            extra_context={
-                **data,
-            },
+            extra_context=cookiecutter_data,
         )
+
+    def should_run(self, data):
+        return not self.ui_dir(data).exists()
 
 
 def add_ui_wizard(data):
@@ -128,13 +140,13 @@ and then I'll ask you a couple of additional questions.
     For which model do you want to generate the ui? 
     """,
                     options=get_available_models(data),
+                    default=next(iter(get_available_models(data))),
                 ),
-                "model_defaults",
+                InputWizardStep(
+                    "url_prefix",
+                    prompt="On which url prefix will the UI reside? The prefix should like /something/: ",
+                    default=lambda data: f"/{data.section}/",
+                ),
             ]
-        ),
-        InputWizardStep(
-            "url_prefix",
-            prompt="On which url prefix will the UI reside? The prefix should like /something/: ",
-            default=f"/{data['ui_name']}/",
         ),
     )
