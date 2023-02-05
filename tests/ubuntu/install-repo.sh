d39eb89552406c2295998a460789750f3f65cc9d
Mirek Simek <miroslav.simek@gmail.com>
2023-02-05 15:03:18 +0100

    patching invenio

--- tests/ubuntu/install-repo.sh
+++ tests/ubuntu/install-repo.sh
@@ -7,3 +7,11 @@ curl -O https://raw.githubusercontent.com/oarepo/oarepo-cli/v11.0/nrp-installer.
 
 # run the repo installer
 bash nrp-installer.sh -p python3 repo --config ${TEST_DATA_DIR}/oarepo.yaml --no-input
+
+
+# patch invenio
+
+# max recursion error in template when invenio-administration templates are registered before invenio-theme templates
+find repo -path '**/invenio_administration/templates/semantic-ui/invenio_theme/header_login.html' | while read ; do
+  mv $REPLY $(dirname $REPLY)/administration_header_login.html
+done
