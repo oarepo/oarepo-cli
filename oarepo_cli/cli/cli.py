cee7c4c2652e5d78bcd47cc4952b319fe196a299
Mirek Simek <miroslav.simek@gmail.com>
2023-03-05 16:39:34 +0100

    working on ui support in cli

--- oarepo_cli/cli/cli.py
+++ oarepo_cli/cli/cli.py
@@ -5,6 +5,7 @@ from oarepo_cli.cli.model import model
 from oarepo_cli.cli.run import run_server
 from oarepo_cli.cli.site import site
 from oarepo_cli.cli.ui import ui
+from oarepo_cli.cli.upgrade import upgrade
 
 
 @click.group()
@@ -17,6 +18,7 @@ run.add_command(site)
 run.add_command(model)
 run.add_command(ui)
 run.add_command(run_server)
+run.add_command(upgrade)
 
 if __name__ == "__main__":
     run()
