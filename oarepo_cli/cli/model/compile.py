6b13a02700394aa741dbd734e32fe4dba61a79cc
Mirek Simek <miroslav.simek@gmail.com>
2023-05-23 15:11:03 +0200

    upgraded model builder to v4

--- oarepo_cli/cli/model/compile.py
+++ oarepo_cli/cli/model/compile.py
@@ -68,34 +68,34 @@ class CompileWizardStep(ModelWizardStep, WizardStep):
         pip_install(
             venv_dir / "bin" / "pip",
             "OAREPO_MODEL_BUILDER_VERSION",
-            "oarepo-model-builder==3.*",
+            "oarepo-model-builder==4.*",
             "https://github.com/oarepo/oarepo-model-builder",
         )
         pip_install(
             venv_dir / "bin" / "pip",
             "OAREPO_MODEL_BUILDER_TESTS_VERSION",
-            "oarepo-model-builder-tests==3.*",
+            "oarepo-model-builder-tests==4.*",
             "https://github.com/oarepo/oarepo-model-builder-tests",
         )
         if self.data.get("use_requests", None) == "yes":
             pip_install(
                 venv_dir / "bin" / "pip",
                 "OAREPO_MODEL_BUILDER_REQUESTS_VERSION",
-                "oarepo-model-builder-requests==3.*",
+                "oarepo-model-builder-requests==4.*",
                 "https://github.com/oarepo/oarepo-model-builder-requests",
             )
         if self.data.get("use_expandable_fields", None) == "yes":
             pip_install(
                 venv_dir / "bin" / "pip",
                 "OAREPO_MODEL_BUILDER_EXPANSIONS_VERSION",
-                "oarepo-model-builder-expansions==3.*",
+                "oarepo-model-builder-expansions==4.*",
                 "https://github.com/oarepo/oarepo-model-builder-expansions",
             )
         if self.data.get("use_files", None) == "yes":
             pip_install(
                 venv_dir / "bin" / "pip",
                 "OAREPO_MODEL_BUILDER_FILES_VERSION",
-                "oarepo-model-builder-files==3.*",
+                "oarepo-model-builder-files==4.*",
                 "https://github.com/oarepo/oarepo-model-builder-files",
             )
 
